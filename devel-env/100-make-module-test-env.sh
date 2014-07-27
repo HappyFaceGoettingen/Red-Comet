a2c98839a88a850f99b4fff4f7f1247e9e30ae0c
gen6scp <gen6scp@gmail.com>
2014-07-27 08:08:47 +0200

    Added devel-env

---
+++ devel-env/100-make-module-test-env.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -x
+
+devel_dir=/var/lib/HappyFace3-devel
+[ ! -e git.conf ] && echo "[git.conf] does not exist!" && exit -1 
+. git.conf
+
+cd $devel_dir
+
+## cert
+[ ! -e cert ] && mkdir -v cert
+chmod 777 $devel_dir/cert
+chmod +t $devel_dir/cert
+sudo ln -sf $devel_dir/cert /var/lib/HappyFace3/
+
+## hf.gridengine & toolkit
+sudo ln -sf $devel_dir/$project_name/hf/gridengine /var/lib/HappyFace3/hf
+sudo ln -sf $devel_dir/$project_name/hf/gridtoolkit /var/lib/HappyFace3/hf
+
+## module
+sudo ln -sf $devel_dir/$project_name/modules/* /var/lib/HappyFace3/modules/
+
+## symlinks for module tests
+ln -sf $devel_dir/$project_name/defaultconfig $devel_dir/$project_name/hf/gridengine/
+ln -sf $devel_dir/$project_name/defaultconfig $devel_dir/$project_name/hf/gridtoolkit/
+ln -sf $devel_dir/$project_name/config $devel_dir/$project_name/hf/gridengine/
+ln -sf $devel_dir/$project_name/config $devel_dir/$project_name/hf/gridtoolkit/
+
+## config
+sudo ln -sf $devel_dir/$project_name/config/categories-enabled/*.cfg /var/lib/HappyFace3/config/categories-enabled/
+sudo ln -sf $devel_dir/$project_name/config/modules-enabled/*.cfg /var/lib/HappyFace3/config/modules-enabled/
+
+## defaultconfig
+sudo rm -v /var/lib/HappyFace3/defaultconfig/happyface.cfg
+sudo ln -s $devel_dir/$project_name/defaultconfig/happyface.cfg /var/lib/HappyFace3/defaultconfig/happyface.cfg
+
+## acquire.py
+sudo ln -s $devel_dir/$project_name/grid_enabled_acquire.py /var/lib/HappyFace3/grid_enabled_acquire.py
+
+
+
+#----------------------------------------
+# Generate certs
+#----------------------------------------
+[ ! -e $HOME/.globus/usercert.pem ] && echo "No [$HOME/.globus/usercert.pem] Usercert!!" && exit -1
+[ ! -e $HOME/.globus/userkey.pem ] && echo "No [$HOME/.globus/userkey.pem] Userkey!!" && exit -1
+
+## copying usercert
+[ ! -e $devel_dir/cert/usercert.pem ] && cp -v $HOME/.globus/usercert.pem $devel_dir/cert/usercert.pem
+
+## generating userkey witout passphrase
+[ ! -e $devel_dir/cert/userkey.nopass.pem ] && openssl rsa -in $HOME/.globus/userkey.pem -out $devel_dir/cert/userkey.nopass.pem
+[ ! -e $devel_dir/cert/userkey.pem ] && ln -sf $devel_dir/cert/userkey.nopass.pem $devel_dir/cert/userkey.pem
+
+## giving proper permissions and ownerships
+sudo chown $USER $devel_dir/cert/usercert.pem
+sudo chown $USER $devel_dir/cert/userkey.nopass.pem
+[ -e $devel_dir/cert/x509up_happyface ] && sudo chown $USER $devel_dir/cert/x509up_happyface
+chmod 644 $devel_dir/cert/usercert.pem
+chmod 400 $devel_dir/cert/userkey.nopass.pem
---
+++ devel-env/200-exec-simple-integration-test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+devel_dir=/var/lib/HappyFace3-devel
+[ ! -e git.conf ] && echo "[git.conf] does not exist!" && exit -1 
+. git.conf
+
+cd $devel_dir
+
+
+#----------------------------------------
+# Generate certs
+#----------------------------------------
+## giving proper permissions and ownerships
+sudo chown happyface3 $devel_dir/cert/usercert.pem
+sudo chown happyface3 $devel_dir/cert/userkey.nopass.pem
+sudo chown happyface3 $devel_dir/cert/userkey.pem
+[ -e $devel_dir/cert/x509up_happyface ] && sudo chown happyface3 $devel_dir/cert/x509up_happyface
+
+
+#----------------------------------------
+# Execute grid_enabled_acuire.py
+#----------------------------------------
+cd /var/lib/HappyFace3
+sudo su happyface3 -c "export PYTHONPATH=/var/lib/HappyFace3; python grid_enabled_acquire.py"
+
+
+#----------------------------------------
+# Restart httpd
+#----------------------------------------
+sudo /etc/init.d/httpd restart
