d3fd121c81f4163229f5873084ab36fb90961b34
hmushegh <m.haykuhi@gmail.com>
2015-09-02 15:35:28 +0200

    Update GridFtpCopyViewer.py

--- modules/GridFtpCopyViewer.py
+++ modules/GridFtpCopyViewer.py
@@ -23,6 +23,11 @@ from hf.gridtoolkit.GridFtpCopyHandler import GridFtpCopyHandler
 from hf.gridtoolkit.Transfers import Transfers
 from hf.gridtoolkit.SpaceTokens import SpaceTokens
 from hf.gridtoolkit.GenerateFile import *
+from hf.module.database import hf_runs
+import time
+import datetime
+
+import cgi, cgitb 
 
 class GridFtpCopyViewer(hf.module.ModuleBase):
     
@@ -47,7 +52,12 @@ class GridFtpCopyViewer(hf.module.ModuleBase):
         Column('localgroupdiskToLocalgroupdisk', TEXT),
         Column('localgroupdiskToProddisk', TEXT),
         Column('localgroupdiskToDatadisk', TEXT),
-    ], [])                
+    ], []),
+        'site_transfers_plot': ([
+        Column('efficiency_plot', TEXT),
+    ], [])
+                        
+                       
     }
          
     __transfer_obj = None
@@ -110,10 +120,10 @@ class GridFtpCopyViewer(hf.module.ModuleBase):
             'dataset': "Hi",
             'status': 1
         }   
-        
+        """
         logging.basicConfig(level=logging.INFO)
         logging.root.setLevel(logging.INFO) 
-          
+        
         # Site 1 to Site 1 transfers
         __transfer_obj = Transfers()
         __spacetoken_obj = SpaceTokens()               
@@ -246,7 +256,7 @@ class GridFtpCopyViewer(hf.module.ModuleBase):
         print "Localgroupdisk:"
         if mkdir_status4:
             __transfer_obj_1_2.setSrcPath(self.config['site1_localgroupdisk_path']) 
-            lg_src_status_1_2,   lg_lg_status_1_2,  lg_prod_status_1_2, lg_data_status_1_2 = self.siteTransfers(__transfer_obj_1_2, __spacetoken_obj_1_2, __grid_ftp_handler_1_2, __genFile_1_2)
+            lg_src_status_1_2, lg_lg_status_1_2, lg_prod_status_1_2, lg_data_status_1_2 = self.siteTransfers(__transfer_obj_1_2, __spacetoken_obj_1_2, __grid_ftp_handler_1_2, __genFile_1_2)
        
         else: 
             print "Failed to create a directory"
@@ -282,7 +292,7 @@ class GridFtpCopyViewer(hf.module.ModuleBase):
         __spacetoken_obj_2_1.setProdDiskPath(self.config['site1_proddisk_path']) 
         __spacetoken_obj_2_1.setDataDiskPath(self.config['site1_datadisk_path'])        
     
-        # Result of the transfers from Site1 to Site1      
+        # Result of the transfers from Site2 to Site1      
         __transfer_obj_2_1.setSrcPath(self.config['site2_scratchdisk_path'])  
         src_src_status_2_1,  src_lg_status_2_1, src_prod_status_2_1, src_data_status_2_1 = self.siteTransfers( __transfer_obj_2_1, __spacetoken_obj_2_1, __grid_ftp_handler_2_1, __genFile_2_1)
                
@@ -296,7 +306,6 @@ class GridFtpCopyViewer(hf.module.ModuleBase):
         self.details_table_db_value_list.append({})
         self.details_table_db_value_list[2] = details_2_1    
        
-        
         obj = GridFtpCopyHandler()
         #Remove files from local path
         obj.rmLocal()  
@@ -304,7 +313,25 @@ class GridFtpCopyViewer(hf.module.ModuleBase):
         self.removeTmpFiles(obj, __transfer_obj.getDstHost(), __transfer_obj.getDstPort(), __spacetoken_obj.getScratchDiskPath(),__spacetoken_obj.getLocalGroupDiskPath(), __spacetoken_obj.getProdDiskPath(), __spacetoken_obj.getDataDiskPath())
         #Remove files from Site2 space tokens
         self.removeTmpFiles(obj, __transfer_obj_1_2.getDstHost(), __transfer_obj.getDstPort(), __spacetoken_obj_1_2.getScratchDiskPath(),__spacetoken_obj_1_2.getLocalGroupDiskPath(), __spacetoken_obj_1_2.getProdDiskPath(), __spacetoken_obj_1_2.getDataDiskPath())
-                                 
+        """
+        #Ploting
+        
+        ok_sp_token_eff1, failed_sp_token_eff1 = self.sqlEfficiency('scratchdiskToScratchdisk', 'OK', self.config['plot_start_date'])
+        ok_sp_token_eff2, failed_sp_token_eff2 = self.sqlEfficiency('localgroupdiskToLocalgroupdisk', 'OK', self.config['plot_start_date'])              
+        
+        ok_sp_token_eff3, failed_sp_token_eff3 = self.sqlEfficiency('scratchdiskToProddisk', 'Error', self.config['plot_start_date'])              
+        ok_sp_token_eff4, failed_sp_token_eff4 = self.sqlEfficiency('scratchdiskToDatadisk', 'Error', self.config['plot_start_date'])
+                
+        
+        ok_status_plot = (ok_sp_token_eff1, ok_sp_token_eff2, ok_sp_token_eff3, ok_sp_token_eff4)
+        failed_status_plot = (failed_sp_token_eff1, failed_sp_token_eff2, failed_sp_token_eff3, failed_sp_token_eff4)
+        
+        details_plot = {}        
+        details_plot['efficiency_plot'] = self.efficiencyPlot(ok_status_plot, failed_status_plot)
+                                       
+        self.details_table_db_value_list_plot.append({})
+        self.details_table_db_value_list_plot[0] = details_plot  
+        
         return data
     
      
@@ -326,16 +353,122 @@ class GridFtpCopyViewer(hf.module.ModuleBase):
 
     def prepareAcquisition(self):        
         self.details_table_db_value_list = []
+        self.details_table_db_value_list_plot = []
              
     def fillSubtables(self, parent_id):
         self.subtables['site_transfers'].insert().execute([dict(parent_id=parent_id, **row) for row in self.details_table_db_value_list])
-           
-    def SQLQuery(self, siteName):
+        self.subtables['site_transfers_plot'].insert().execute([dict(parent_id=parent_id, **row) for row in self.details_table_db_value_list_plot])
+          
+    def sqlQuery(self, siteName):
         max_id_number1_uberftp = func.max(self.subtables['site_transfers'].c.id).select().where(self.subtables['site_transfers'].c.siteName == str(siteName)).execute().scalar()        
         details1 = self.subtables['site_transfers'].select().where(self.subtables['site_transfers'].c.id == max_id_number1_uberftp).execute().fetchall()
-        return map(dict, details1)        
+        return map(dict, details1)  
     
-                       
+    def sqlQuery_plot(self):        
+        plot = self.subtables['site_transfers_plot'].select().where(self.subtables['site_transfers_plot'].c.parent_id==self.dataset['id']).execute().fetchall()
+        return map(dict, plot) 
+    
+    def showSqlErroMsg(self, db_space_token1, db_space_token2, msg, date_str):
+        """
+        SELECT DISTINCT sub_grid_ftp_copy_viewer_site_transfers."scratchdiskToScratchdisk" 
+        FROM sub_grid_ftp_copy_viewer_site_transfers INNER JOIN hf_runs 
+        ON hf_runs.id = sub_grid_ftp_copy_viewer_site_transfers.id 
+        WHERE hf_runs.time >= '2015-06-05' AND scratchdiskToScratchdisk NOT LIKE '%OK%'  
+        
+        UNION 
+        
+        SELECT DISTINCT sub_grid_ftp_copy_viewer_site_transfers."localgroupdiskToScratchdisk" 
+        FROM sub_grid_ftp_copy_viewer_site_transfers INNER JOIN hf_runs 
+        ON hf_runs.id = sub_grid_ftp_copy_viewer_site_transfers.id 
+        WHERE hf_runs.time >= '2015-06-05' AND localgroupdiskToScratchdisk NOT LIKE '%OK%';
+        """
+        
+        range = "hf_runs.time >='" + date_str + "'"       
+        failed_sp_token_str1 = db_space_token1 + " NOT LIKE '%" + msg + "%'"
+        failed_sp_token_str2 = db_space_token2 + " NOT LIKE '%" + msg + "%'"
+        
+        query1 =  self.subtables['site_transfers'].join(hf_runs, hf_runs.c.id== self.subtables['site_transfers'].c.id)
+        failed_status1 = select([getattr(self.subtables['site_transfers'].c, db_space_token1)]).select_from(query1).distinct().where(and_(range, failed_sp_token_str1))#.execute().fetchall()
+        
+        query2 =  self.subtables['site_transfers'].join(hf_runs, hf_runs.c.id== self.subtables['site_transfers'].c.id)
+        failed_status2 = select([getattr(self.subtables['site_transfers'].c, db_space_token2)]).select_from(query2).distinct().where(and_(range, failed_sp_token_str2))#.execute().fetchall()
+               
+        result = failed_status1.union(failed_status2).execute().fetchall()
+               
+        return result
+        
+        
+        
+    def sqlEfficiency(self, db_space_token, msg, date_str):
+      
+        """
+        SELECT sub_grid_ftp_copy_viewer_site_transfers.id, sub_grid_ftp_copy_viewer_site_transfers.parent_id, sub_grid_ftp_copy_viewer_site_transfers."siteName", sub_grid_ftp_copy_viewer_site_transfers."scratchdiskToScratchdisk", sub_grid_ftp_copy_viewer_site_transfers."scratchdiskToLocalgroupdisk", sub_grid_ftp_copy_viewer_site_transfers."scratchdiskToProddisk", sub_grid_ftp_copy_viewer_site_transfers."scratchdiskToDatadisk", sub_grid_ftp_copy_viewer_site_transfers."localgroupdiskToScratchdisk", sub_grid_ftp_copy_viewer_site_transfers."localgroupdiskToLocalgroupdisk", sub_grid_ftp_copy_viewer_site_transfers."localgroupdiskToProddisk", sub_grid_ftp_copy_viewer_site_transfers."localgroupdiskToDatadisk" 
+        FROM sub_grid_ftp_copy_viewer_site_transfers JOIN hf_runs ON hf_runs.id = sub_grid_ftp_copy_viewer_site_transfers.id 
+        WHERE hf_runs.time >= '2015-06-01' AND scratchdiskToScratchdisk LIKE '%OK%'; 
+                
+        SELECT count(sub_grid_ftp_copy_viewer_site_transfers.id) AS count_1 FROM hf_runs INNER JOIN sub_grid_ftp_copy_viewer_site_transfers ON   hf_runs.id =sub_grid_ftp_copy_viewer_site_transfers.id WHERE sub_grid_ftp_copy_viewer_site_transfers.scratchdiskToScratchdisk LIKE '%OK%' AND hf_runs.time >=  '2015-08-01';
+                
+        SELECT sub_grid_ftp_copy_viewer_site_transfers.id 
+        FROM sub_grid_ftp_copy_viewer_site_transfers JOIN hf_runs ON hf_runs.id = sub_grid_ftp_copy_viewer_site_transfers.id 
+        WHERE hf_runs.time >= '2015-06-01' AND scratchdiskToScratchdisk LIKE '%OK%';
+
+        """
+        range = "hf_runs.time >='" + date_str + "'"
+        ok_compr = db_space_token + " LIKE '%" + msg + "%'"
+               
+        query1 =  self.subtables['site_transfers'].join(hf_runs, hf_runs.c.id== self.subtables['site_transfers'].c.id)
+        ok_status = select([self.subtables['site_transfers'].c.id]).select_from(query1).where(and_(range, ok_compr)).execute().fetchall()
+ 
+        failed_compr = db_space_token + " NOT LIKE '%" + msg + "%'"
+        query2 =  self.subtables['site_transfers'].join(hf_runs, hf_runs.c.id== self.subtables['site_transfers'].c.id)
+        failed_status = select([self.subtables['site_transfers'].c.id]).select_from(query2).where(and_(range, failed_compr)).execute().fetchall()
+   
+        return len(ok_status), len(failed_status)
+    
+    def autolabel(self, ax, rects):
+            for rect in rects:
+                h = rect.get_height()
+                ax.text(rect.get_x()+rect.get_width()/2., 1.05*h, '%d'%int(h),
+                        ha='center', va='bottom')
+    
+    
+    def efficiencyPlot(self, arry1, arry2):
+        import numpy as np
+        import matplotlib.pyplot as plt
+                
+        N = len(arry1)
+        ind = np.arange(1, N+1)
+        width = 0.30
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)  
+        
+        bar1 = ax.bar(ind, arry1, width, color='#4FD516' , yerr = 0.001)    
+        bar2 = ax.bar(ind+width, arry2, width, color='#FF6464', yerr = 0.001) 
+       
+        ax.set_xlabel('Space tokens')
+        ax.set_ylabel('DB records count')
+        ax.set_title('GoeGrid space tokens efficiency \n starting from '  + self.config['plot_start_date'])
+        ax.set_xticks(ind + width)        
+        ax.set_xticklabels(('SrcDisk', 'LgDisk', 'ProdDisk', 'DataDisk'))
+        
+        if max(arry1) >= max(arry2):            
+            plt.yticks(np.arange(0,max(arry1)*2, max(arry1)//3))
+        else:
+            plt.yticks(np.arange(0,max(arry2)*2,max(arry2)//3 ))
+        
+        ax.legend((bar1[0], bar2[0]), ('Succeed', 'Failed'))
+        
+        self.autolabel(ax, bar1)
+        self.autolabel(ax, bar2)
+        
+        plt.show()
+
+        fig.savefig(hf.downloadService.getArchivePath(self.run, self.instance_name + "db_eff.png"), dpi=60)
+        plt.close()       
+        dest = "/"+hf.downloadService.getArchivePath(self.run, self.instance_name + "db_eff.png")
+        return dest
+   
     def getTemplateData(self):        
         data = hf.module.ModuleBase.getTemplateData(self)        
         self.dataset['site1Name'] = self.config['site1_name']                
@@ -351,14 +484,22 @@ class GridFtpCopyViewer(hf.module.ModuleBase):
         self.dataset['site2_space_token_proddisk'] = self.config['site2_space_token_proddisk']
         self.dataset['site2_space_token_datadisk'] = self.config['site2_space_token_datadisk']
         
+        self.dataset['plot_start_date'] = self.config['plot_start_date']
+        
         self.dataset['ok_status'] = ['OK', 'SpaceException', 'FileExists']
         self.dataset['ok_status_prod_data'] = ['Permission denied', 'Authorization', 'No permission']
-        self.dataset['error_status'] = ['Failed', 'Error', 'No match', 'Permission denied']
-        self.dataset['error_status_prod_data'] = ['OK', 'SpaceException', 'FileExists']
+        self.dataset['error_status'] = ['Failed', 'Error', 'No match', 'Permission denied', 'Cannot create a directory', 'Remote server has disconnected', 'Transfer in progress', 'timeout', 'is not a directory']
+        self.dataset['error_status_prod_data'] = ['OK', 'SpaceException', 'FileExists', 'Cannot create a directory', 'Remote server has disconnected', 'Transfer in progress', 'timeout']
                         
-        data['site1_site1_transfers'] = self.SQLQuery(self.config['site1_name'])  
-        data['site1_site2_transfers'] = self.SQLQuery(self.config['site2_name1'])     
-        data['site2_site1_transfers'] = self.SQLQuery(self.config['site2_name2'])
-                
+        data['site1_site1_transfers'] = self.sqlQuery(self.config['site1_name'])  
+        data['site1_site2_transfers'] = self.sqlQuery(self.config['site2_name1'])     
+        data['site2_site1_transfers'] = self.sqlQuery(self.config['site2_name2'])
+        data['site_transfers_plot'] = self.sqlQuery_plot()
+        
+        data['show_err1'] = self.showSqlErroMsg('scratchdiskToScratchdisk', 'localgroupdiskToScratchdisk', 'OK', self.config['plot_start_date'])
+        data['show_err2'] = self.showSqlErroMsg('scratchdiskToLocalgroupdisk', 'localgroupdiskToLocalgroupdisk', 'OK', self.config['plot_start_date']) 
+        data['show_err3'] = self.showSqlErroMsg('scratchdiskToProddisk', 'localgroupdiskToProddisk', 'Error', self.config['plot_start_date']) 
+        data['show_err4'] = self.showSqlErroMsg('scratchdiskToDatadisk', 'localgroupdiskToDatadisk', 'Error', self.config['plot_start_date'])  
+        
         
         return data
